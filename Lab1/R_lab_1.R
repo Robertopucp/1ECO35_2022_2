7f79a43b015aa7fc07298067a756da3e979e6309
Roberto Mendoza <54970712+Robertopucp@users.noreply.github.com>
2022-08-24 21:15:29 -0500

    #

--- Lab1/R_lab_1.R
+++ Lab1/R_lab_1.R
@@ -1,18 +1,8 @@
-a <- 3
+################  laboratorio 1 ############################
+## Curso: Laboratorio de R y Python ###########################
+## @author: Roberto Mendoza 
 
-a <- b <- 4
-
-a
-
-a <- b = 4
-
-a <- b <-5
-a
-
-a1 <- 52
-print(a1)
-typeof(a1)  
-class(a1)
+" Laboratorio 1 Python"
 
 # "typeof" how the object is stored and "class" is the type of object 
 
@@ -20,9 +10,14 @@ class(a1)
 
 # integer, single precision and double precision
 
+## ---------------------------------------------------------------------
+
+"Types of variables"
+
 a2 <- 3.1614
 print(a2)
 typeof(a2)
+class(a2)
 
 a2 <- as.integer(a2)
 typeof(a2)
@@ -31,8 +26,7 @@ class(a2)
 b1 <- 10000
 typeof(b1)
 
-b2 <- 8
-typeof(b2)
+" ######## string  #############"
 
 c1 <- "My first python code"
 print(c1)
@@ -40,35 +34,30 @@ typeof(c1)
 class(c1)
 
 c1 <- "First python code"
-cat(c1, '\n','\n')
-
-c2 <- 'Lab1 Pucp'
+c2 <- 'at R y python Class'
 cat(c1," : ",c2)
 
-#install.packages("devtools")
 
-library(glue)
-a <- glue('{c1} : semester 2022-1')
-a
-
-typeof(a)
-class(a)
+a <- paste0(c1,' : semester 2022-1')
+print(a)
 
 d <- 2022
-glue('{c1} : semester {d}-1')
-
-sprintf(" %s. : %s.-1", c1, d)
+paste0(c1,' : semester ',d, '-1')
 
 
 #first character
+
 cat('Fisrt letter is :', substr(c1, 1,1)  )
 
 #first word
 cat('Fisrt word is :', substr(c1, 1,5)  )
 
-## 2.0 Logical variables
 
-It’s used to represent the truth value of an expression.
+"cat just contatenate characters to print"
+
+## ---------------------------------------------------------------------
+
+"2.0 Logical variables"
 
 a == a
 
@@ -76,7 +65,6 @@ a == a
 
 z1 <- (1==1)
 typeof(z1)
-class(z1)
 
 z1 <- as.integer(z1)
 print(z1)
@@ -89,144 +77,22 @@ z3 <- (100 != 100)
 z3 <- as.integer(z3)
 typeof(z3)
 
-## 3.0 List
-
-
-L1 <- list()
-
-dis1 <- list('ATE', 'BARRANCO','BREÑA', 'CALLAO', 'CARABAYLLO')
-dis1
-
-typeof(dis1)
-class(dis1)
-
-dis2 <- list('ATE', 'BARRANCO','BREÑA', 'CALLAO', 'CARABAYLLO','CHACLACAYO','CHORRILLOS','CIENEGUILLA'
-             ,'COMAS','EL_AGUSTINO','INDEPENDENCIA')
-dis2
-
-dis1[1]
-
-typeof(dis1[1])
-class(dis1[1])
-
-#double [] to get a singular object 
-
-typeof(dis1[[1]])
-
-#dis1 <- list('ATE', 'BARRANCO','BREÑA', 'CALLAO', 'CARABAYLLO')
-
-dis1[2:5]
-
-dis1[-1] # drop first element
-
-dis1[-5]
-
-num <- list(13,5,5,8,9,10,5,8,13,1,20)
-#print(num)
-
-length(num) # number of elements 
-
-typeof(num[[11]])
-print(num[[11]])
-
-num[[11]]
-
-# unlist to order, then covert list again 
-
-a <- sort(unlist(num))
-print(list(a))
-a[11]
-
-max(unlist(num))
-
-#dis2 <- list('ATE', 'BARRANCO','BREÑA', 'CALLAO', 'CARABAYLLO','CHACLACAYO','CHORRILLOS','CIENEGUILLA'
-#            ,'COMAS','EL_AGUSTINO','INDEPENDENCIA')
-
-length(dis2)
-
-#add a single element
-
-num <- append(num, 102)
-
-num
-
-# add a list 
-
-num2 <- list(10,20,30)
-append(num, num2)
-
-
-cat("Suma:", sum(unlist(num)),'\n', "Minimo:", min(unlist(num)), '\n', "Maximo:", max(unlist(num)))
-
-# add type of objects
-
-new_list <- list(c(1,2,3),         
-                 "R ", 
-                 120)
-print(new_list)
-
-## List part 2 - dictionary
-
-# using list as dictionary in python 
-
-Postal_code <- list(Majes = 40520, Mollendo = 40701, Islay = 40704, Cotahuasi = 40801, Alca = 40802 )
-print(Postal_code)
-
-Postal_code$Alca
-
-# add package to make operations 
-
-install.packages("rlist")
-library(rlist)
-
-
-# empty list 
-
-a <- list()
-
-#make empty
-Postal_code <- list.remove(Postal_code)
-Postal_code
-
-Postal_code <- list(Majes = 40520, Mollendo = 40701, 
-                    Islay = 40704, Cotahuasi = 40801, Alca = 40802 )
-print(Postal_code)
-
-Postal_code$Islay
-
-a <- list.remove(Postal_code,"Islay")
-a
-
-# add new key
-
-Postal_code <- append(Postal_code, list(CHARCANA =  40803 ))
-Postal_code
-
-Postal_code <- append(Postal_code, list(LOMAS =  list(UBIGEO = 40311, Poverty_Rate = "18.2%", Population = "20 mil")))
-
-Postal_code$LOMAS$Population
-
-Postal_code$LOMAS$Poverty_Rate
+## ---------------------------------------------------------------------
 
 ### 3.0 Array and Matrices
 
 # 1D numeric array
 
-a <- c(1,2,3,4,5)
-
-
+a <- c(1,2,3,4,"Perú")
 print(a)
-typeof(a)
-class(a)
+
+a <- c(1,2,3,4)
+a <- append(a, 5)
+b <-  rep(2,3) # repeat 2 3 times 
+append(a,b)
 
 print(mean(a))
 print(sd(a))
-
-Standar deviation in R
-
-\begin{equation}
-sd(x) = \sqrt{\frac{\sum(x_i-\bar{x})^2}{N-1}}
-\end{equation}
 
 # take care standar deviation formula
 
@@ -240,10 +106,9 @@ M <- matrix(c(1,2,3,4,5,6), nrow = 2, byrow =TRUE)
 
 print(M)
 typeof(M)
+dim(M)
 
-#colMeans(M)
-#rowMeans(M)
-#cat(colMeans(M, dim = 1), "\n", colMeans(M, dim = 2))
+# Matrix by colum
 
 M <- matrix(c(1,2,3,4,5,6), nrow = 2, byrow =FALSE)
 print(M)
@@ -254,19 +119,18 @@ dim(M)[1]
 
 # Create a 1D NumPy array with values from 0 to 20 (exclusively) incremented by 2.5:
 
-y <- matrix(seq(0,19,2))
-class(y)
+y <- seq(0,19,2)
 print(y)
 typeof(y)
 
-y[2]
+y[1]
 
-y <- matrix(seq( 1, 10))
+# second example 
+
+y <- seq( 1, 10)
 print(y)
 
-for (i in y) {
-  print(i+1)    
-}
+# Matrix
 
 A <- matrix(c(seq(0, 9), seq( 10, 19), seq( 30, 39), seq( -20, -11), seq( 2, 20,2)), nrow = 5, byrow =TRUE)
 A
@@ -275,7 +139,7 @@ A[2:4,] # rows selecrtion
 
 A[,1:6]  # columns selecrtion
 
-A[,-c(2,3)]
+A[,-c(2,3)] # drop columns 
 
 # Join matrix and special Matrix
 
@@ -286,11 +150,15 @@ print(M1)
 M2 <- matrix(1,8, 4) 
 print(M2)
 
+# horizontal stack 
+
 M3 <- cbind(M1,M2)
 M3
 
 M4 = matrix(c(2,2,3,4,5,1,1,5,5,9,8,2), nrow =2, byrow = TRUE)
 print(M4)
+
+# vertical stack 
 
 M5 <- rbind(M3,M4)
 
@@ -300,22 +168,27 @@ M5
 
 t(M5)
 
+# Matrix Identity
 
 I <- diag(8)
 print(I)
 
+# Reshape
 I3 <- matrix(I, nrow = 32, ncol = 2)
 print(I3)
 typeof(I3)
 
+## ---------------------------------------------------------------------
+
+### 3.0 OLS
+
 set.seed(756)
 
-#x1 = runif(500, min = 0, max = 1)
+
 x1 <- runif(500)
 x2 <- runif(500)
 x3 <- runif(500)
 x4 <- runif(500)
-#random.seed(123)
 e <- rnorm(500)
 
 # Poblacional regression (Data Generating Process GDP)
