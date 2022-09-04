712fc274444b3ab6c04fd31d4f416cc47bc997e6
Fernando Guerrero <111617324+FerGuerrero23@users.noreply.github.com>
2022-09-03 19:48:35 -0500

    Update Grupo_5_R.R

--- Trabajo_grupal/WG1/Grupo_5_R.R
+++ Trabajo_grupal/WG1/Grupo_5_R.R
@@ -1,33 +1,36 @@
+#Pregunta 2
+
+
 library(dplyr) 
 library(tidyr)
 library(readxl)
 
 
 set.seed(188) #Luego, definimos la random seed 188 
-v1 =sample(500)#Posteriormente, creamos x que ser· un vector 1x20 con valores entre 0 y 500
+v1 =sample(500)#Posteriormente, creamos x que ser√° un vector 1x20 con valores entre 0 y 500
 x= sample(500,20)
 print(x) #Comprobamos la matriz x
 
-y=list()#Ahora, para el punto 1.1, definimos el vector y que tendr· como componentes a los valores de x
+y=list()#Ahora, para el punto 1.1, definimos el vector y que tendr√° como componentes a los valores de x
 i <- length(x)
-if (i > 0 and i <= 100) #Si i entre 0 y 100, el componente 1xi adoptar· el valor de i^0.5
+if (i > 0 and i <= 100) #Si i entre 0 y 100, el componente 1xi adoptar√° el valor de i^0.5
 {y.append(i**0.5)}
-{else: y.append(i**0)} #Caso contrario, se computar· el valor de 1
+{else: y.append(i**0)} #Caso contrario, se computar√° el valor de 1
 
 print(y) #Comprobamos la matriz y 
 
-z=list() #Ahora, para el punto 1.2, definimos el vector z que tendr· como componentes a los valores de x
+z=list() #Ahora, para el punto 1.2, definimos el vector z que tendr√° como componentes a los valores de x
 i <- length(x):
-  if  i > 100 and i <= 300: #Si i entre 100 y 300, el componente 1xi adoptar· el valor de i-5
+  if  i > 100 and i <= 300: #Si i entre 100 y 300, el componente 1xi adoptar√° el valor de i-5
   z.append(i-5)
-else: z.append(i**0) #Caso contrario, se computar· el valor de 1
+else: z.append(i**0) #Caso contrario, se computar√° el valor de 1
 
 print(z) #Comprobamos la matriz z
 
-k=() #Ahora, para el punto 1.3, definimos el vector k que tendr· como componentes a los valores de x
+k=() #Ahora, para el punto 1.3, definimos el vector k que tendr√° como componentes a los valores de x
 i <- length(x):
-  if  i > 300 and i <= 500: #Si i entre 300 y 500, el componente 1xi adoptar· el valor de 50
+  if  i > 300 and i <= 500: #Si i entre 300 y 500, el componente 1xi adoptar√° el valor de 50
   k.append(50)
-else: k.append(i**0) #Caso contrario, se computar· el valor de 1
+else: k.append(i**0) #Caso contrario, se computar√° el valor de 1
 
 print(k) #Comprobamos la matriz z
